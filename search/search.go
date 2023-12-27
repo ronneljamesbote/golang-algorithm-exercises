4b643fb2509e6fffd2e96d46e883d8d512cd6096
Ronnel James Miranda Bote <ronneljames.miranda.bote@gmail.com>
2023-12-27 21:05:17 +0300

    Add binary search recursive version with test

--- search/search.go
+++ search/search.go
@@ -28,3 +28,23 @@ func BinarySearchLoop(needle int32, haystack []int32) (int32, error) {
 
 	return 0, errors.New("Needle not found")
 }
+
+func BinarySearchRecurse(needle int32, haystack []int32, min int32, max int32) (int32, error) {
+	var mid int32 = (min + max) / 2
+
+	var curr int32 = haystack[mid]
+
+	if min > max {
+		return 0, errors.New("Needle not found")
+	}
+
+	if curr == needle {
+		return int32(mid), nil
+	}
+
+	if needle < curr {
+		return BinarySearchRecurse(needle, haystack, min, mid-1)
+	}
+
+	return BinarySearchRecurse(needle, haystack, mid+1, max)
+}
--- search/search_test.go
+++ search/search_test.go
@@ -26,3 +26,16 @@ func TestBinarySearchLoop(t *testing.T) {
 		}
 	}
 }
+
+func TestBinarySearchRecurse(t *testing.T) {
+	for v, i := range binarySearchCheck {
+		var want int32 = i
+		var find int32 = v
+
+		result, err := BinarySearchRecurse(find, binarySearchNumbers, 0, int32(len(binarySearchNumbers)))
+
+		if err != nil || result != want {
+			t.Fatalf(`BinarySearchRecurse(%d) = %d, %d given with error %v`, find, want, result, err)
+		}
+	}
+}
